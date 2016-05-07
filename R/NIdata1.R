cb01d1858c2f1ef1ee3c53842bedac9e3dc4e932
Jens Åström <jenast@gmail.com>
2016-05-07 18:55:51 +0200

    added example dataset

---
+++ R/NIdata1.R
@@ -0,0 +1,13 @@
+#' Dummy dataset
+#'
+#' A dataset of 200 random numbers put in a dataframe
+#'
+#' @format A data frame with 40 rows and 5 columns:
+#' \describe{
+#'   \item{column1}{random numbers}
+#'   \item{column1}{random numbers}
+#'   \item{column1}{random numbers}
+#'   \item{column1}{random numbers}
+#'   \item{column1}{random numbers}
+#'   }
+"NIdata1"
