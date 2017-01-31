0a507c96168cb0d6201e1070e0b8b81e8abdfafb
jenast <jenast@gmail.com>
2017-01-31 14:06:02 +0100

    Added example datasets. Not all datasets have help files yet.

---
+++ R/BSunits.R
@@ -0,0 +1,13 @@
+#' Example dataset of basic spatial units
+#'
+#' A dataset of four basic spatial units to be used in the examples
+#'
+#' @format A data frame with 4 rows and 5 columns:
+#' \describe{
+#'   \item{BasicunitID}{Numerical ID of units}
+#'   \item{Basicunit}{Character names of units}
+#'   \item{Area}{Area of units in square meters}
+#'   \item{NIarea1}{Character, indicating the top most level in the spatial hierarchy of the areal units}
+#'   \item{NIarea2}{Character, indicating the second level in the spatial hierarchy of the areal units}
+#'   }
+"BSunits"
