e3ea46abef3fe2de59e564363df7eb951ae84206
Jens Åström <jens.astrom@nina.no>
2019-10-11 16:05:16 +0200

    added inst

---
+++ inst/doc/DatabaseAndDistributions.R
@@ -0,0 +1,3 @@
+### R code from vignette source 'DatabaseAndDistributions.Rnw'
+### Encoding: ISO8859-1
+
---
+++ inst/doc/NatureIndexCalculation.R
@@ -0,0 +1,3 @@
+### R code from vignette source 'NatureIndexCalculation.Rnw'
+### Encoding: ISO8859-1
+
